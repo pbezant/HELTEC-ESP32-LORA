d5910ed8b50e441bda2e21cbca23e015f453e1f1
Preston B <bezantster@gmail.com>
2025-02-24 11:15:14 -0600

    Fix buffer size and key configuration in LoRaWAN setup
    - Correct buffer size for nonces in joinNetwork()
    - Clean up commented key configurations in secrets.h
    - Remove unused commented code and redundant lines

--- secrets.h
+++ secrets.h
@@ -6,12 +6,20 @@ extern const uint64_t devEui = 0x70B3D57ED8003DF4;  // Device EUI from TTN conso
 // Define keys as hex strings
 extern const char* appKey = "893C390D8EA8C6E69BCAA5843548B2C6";
 extern const char* nwkKey = "893C390D8EA8C6E69BCAA5843548B2C6";
-// // The keys need to be byte arrays
+// // // The keys need to be byte arrays
 // extern const uint8_t appKey[16] = { 0xC0, 0x5B, 0xB0, 0x09, 0x87, 0x03, 0x69, 0x02, 
 //                                  0xC5, 0xAF, 0xBD, 0x3F, 0x6A, 0x55, 0xA3, 0xCF };
 // extern const uint8_t nwkKey[16] = { 0xC0, 0x5B, 0xB0, 0x09, 0x87, 0x03, 0x69, 0x02, 
 //                                  0xC5, 0xAF, 0xBD, 0x3F, 0x6A, 0x55, 0xA3, 0xCF };
 //master
+// extern const uint64_t appEui = 0xEABB82D8689A30D7;  // Application Join EUI from TTN console
+// extern const uint64_t joinEui = appEui;
+// extern const uint64_t devEui = 0x70B3D57ED8003FD8;  // Device EUI from TTN console
+// // Define keys as hex strings
+// extern const char* appKey = "C05BB00987036902C5AFBD3F6A55A3CF";
+// extern const char* nwkKey = "C05BB00987036902C5AFBD3F6A55A3CF";
+// // // The keys need to be byte arrays
+
 // extern const uint64_t appEui = "EABB82D8689A30D7";  // Application Join EUI from TTN console
 // extern const uint64_t joinEui = appEui;
 // extern const uint64_t devEui = "70B3D57ED8003FD8";  // Device EUI from TTN console
