8501545954348bb34c8f0fc86d361ea07f059cb4
Preston Bezant <me@prestonbezant.com>
2025-02-25 00:27:05 -0600

    Update LoRaWAN device credentials in secrets.h
    - Modify AppEUI, DevEUI, AppKey, and NwkKey values
    - Ensure credentials match current TTN console configuration

--- secrets.h
+++ secrets.h
@@ -1,11 +1,11 @@
 // #include "LoRaCredentials.h"
 //dev
-extern const uint64_t appEui = 0xEABB82D8689A30D7;  // Application Join EUI from TTN console
+extern const uint64_t appEui = 0x70B3D57ED8004069;  // Application Join EUI from TTN console
 extern const uint64_t joinEui = appEui;
-extern const uint64_t devEui = 0x70B3D57ED8003DF4;  // Device EUI from TTN console
+extern const uint64_t devEui = 0x70B3D57ED8004069;  // Device EUI from TTN console
 // Define keys as hex strings
-extern const char* appKey = "893C390D8EA8C6E69BCAA5843548B2C6";
-extern const char* nwkKey = "893C390D8EA8C6E69BCAA5843548B2C6";
+extern const char* appKey = "0EF9A3826DD27B1F46D82048EE598998";
+extern const char* nwkKey = "0EF9A3826DD27B1F46D82048EE598998";
 // // // The keys need to be byte arrays
 // extern const uint8_t appKey[16] = { 0xC0, 0x5B, 0xB0, 0x09, 0x87, 0x03, 0x69, 0x02, 
 //                                  0xC5, 0xAF, 0xBD, 0x3F, 0x6A, 0x55, 0xA3, 0xCF };
